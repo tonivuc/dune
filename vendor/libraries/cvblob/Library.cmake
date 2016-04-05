d3aa8f474416b09251c590363e0a3323cb6a1123
Pedro Gonçalves <pedro@lsts.pt>
2016-04-05 20:27:51 +0100

    Vendor: cvblob: add restriction if no opencv

--- vendor/libraries/cvblob/Library.cmake
+++ vendor/libraries/cvblob/Library.cmake
@@ -1,3 +1,4 @@
+if(NOT DUNE_USING_OPENCV)
 file(GLOB DUNE_CVBLOB_FILES
   vendor/libraries/cvblob/*.cpp)
   
@@ -7,3 +8,4 @@ set_source_files_properties(${DUNE_CVBLOB_FILES}
   PROPERTIES COMPILE_FLAGS "${DUNE_CXX_FLAGS} ${CVBLOB_CXX_FLAGS}")
 
 list(APPEND DUNE_VENDOR_FILES ${DUNE_CVBLOB_FILES})
+endif(NOT DUNE_USING_OPENCV)
