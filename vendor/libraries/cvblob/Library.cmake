9e67336b2c8a8830797aa20d770e611ac363be23
Pedro Gonçalves <pedro@lsts.pt>
2016-04-05 20:45:12 +0100

    Vendor: cvblob: fix restriction if no opencv

--- vendor/libraries/cvblob/Library.cmake
+++ vendor/libraries/cvblob/Library.cmake
@@ -1,11 +1,14 @@
-if(NOT DUNE_USING_OPENCV)
-file(GLOB DUNE_CVBLOB_FILES
-  vendor/libraries/cvblob/*.cpp)
-  
-set(CVBLOB_CXX_FLAGS "-Wreturn-type -Wunused-but-set-variable -Wsign-compare -Wtype-limits ")
+if(OPENCV)
+  CHECK_LIBRARY_EXISTS(opencv_core cvGetImage "" HAVE_LIB_OPENCV)
+  if(HAVE_LIB_OPENCV)
+		file(GLOB DUNE_CVBLOB_FILES
+			vendor/libraries/cvblob/*.cpp)
+		
+		set(CVBLOB_CXX_FLAGS "-Wreturn-type -Wunused-but-set-variable -Wsign-compare -Wtype-limits ")
 
-set_source_files_properties(${DUNE_CVBLOB_FILES}
-  PROPERTIES COMPILE_FLAGS "${DUNE_CXX_FLAGS} ${CVBLOB_CXX_FLAGS}")
+		set_source_files_properties(${DUNE_CVBLOB_FILES}
+			PROPERTIES COMPILE_FLAGS "${DUNE_CXX_FLAGS} ${CVBLOB_CXX_FLAGS}")
 
-list(APPEND DUNE_VENDOR_FILES ${DUNE_CVBLOB_FILES})
-endif(NOT DUNE_USING_OPENCV)
+		list(APPEND DUNE_VENDOR_FILES ${DUNE_CVBLOB_FILES})
+	endif(HAVE_LIB_OPENCV)
+endif(OPENCV)
