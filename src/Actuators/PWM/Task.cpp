cac709e25a7b882e121b0102e535a2e16ec1f9d7
Pedro Gonçalves <pedro@lsts.pt>
2016-06-17 15:20:00 +0100

    Actuators/PWM: update id of servo.

--- src/Actuators/PWM/Task.cpp
+++ src/Actuators/PWM/Task.cpp
@@ -41,7 +41,7 @@ namespace Actuators
   {
     using DUNE_NAMESPACES;
 
-    static const unsigned int c_max_pwm = 4;
+    static const unsigned int c_max_pwm = 3;
     static const std::string c_mode_operation[] = {"Gimbal", "Drop", "Sample"};
 
     struct Task: public DUNE::Tasks::Task
@@ -56,12 +56,12 @@ namespace Actuators
       };
 
       Arguments m_args;
+      //! Servo 0
+      ServoPwm* m_servo_0;
       //! Servo 1
-      ServoPwm* m_servo1;
+      ServoPwm* m_servo_1;
       //! Servo 2
-      ServoPwm* m_servo2;
-      //! Servo 3
-      ServoPwm* m_servo3;
+      ServoPwm* m_servo_2;
       //! State of update msg servo position
       bool updateMsg;
       //! Value of servo position in deg
@@ -78,11 +78,11 @@ namespace Actuators
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
       DUNE::Tasks::Task(name, ctx),
-      m_servo1(NULL),
-      m_servo2(NULL),
-      m_servo3(NULL)
+      m_servo_0(NULL),
+      m_servo_1(NULL),
+      m_servo_2(NULL)
       {
-        for(unsigned int i = 1; i < c_max_pwm; ++i)
+        for(unsigned int i = 0; i < c_max_pwm; ++i)
         {
           std::string option = String::str("PinOut %u", i);
           param(option, m_args.port_io[i])
@@ -104,26 +104,26 @@ namespace Actuators
         setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
         if ( m_args.operation_mode == c_mode_operation[0])
         {
-          m_servo1 = new ServoPwm(this, m_args.port_io[1], 1.308997, true);
-          m_servo1->start();
-          m_servo2 = new ServoPwm(this, m_args.port_io[2], 1.308997, true);
-          m_servo2->start();
+          m_servo_0 = new ServoPwm(this, m_args.port_io[0], 1.308997, true);
+          m_servo_0->start();
+          m_servo_1 = new ServoPwm(this, m_args.port_io[1], 1.308997, true);
+          m_servo_1->start();
           m_wrong_mode = false;
         }
         else if (m_args.operation_mode == c_mode_operation[1])
         {
-          m_servo3 = new ServoPwm(this, m_args.port_io[3], 1.308997, false);
-          m_servo3->start();
+          m_servo_2 = new ServoPwm(this, m_args.port_io[2], 1.308997, false);
+          m_servo_2->start();
           m_wrong_mode = false;
         }
         else if (m_args.operation_mode == c_mode_operation[2])
         {
-          m_servo1 = new ServoPwm(this, m_args.port_io[1], 3.141593, false);
-          m_servo1->start();
-          m_servo2 = new ServoPwm(this, m_args.port_io[2], 3.141593, false);
-          m_servo2->start();
-          m_servo3 = new ServoPwm(this, m_args.port_io[3], 3.141593, false);
-          m_servo3->start();
+          m_servo_0 = new ServoPwm(this, m_args.port_io[0], 3.141593, false);
+          m_servo_0->start();
+          m_servo_1 = new ServoPwm(this, m_args.port_io[1], 3.141593, false);
+          m_servo_1->start();
+          m_servo_2 = new ServoPwm(this, m_args.port_io[2], 3.141593, false);
+          m_servo_2->start();
           m_wrong_mode = false;
         }
         else
@@ -134,23 +134,23 @@ namespace Actuators
       void
       onResourceRelease(void)
       {
-        if (m_servo1 != NULL)
+        if (m_servo_0 != NULL)
         {
-          m_servo1->stopAndJoin();
-          delete m_servo1;
-          m_servo1 = NULL;
+          m_servo_0->stopAndJoin();
+          delete m_servo_0;
+          m_servo_0 = NULL;
         }
-        if (m_servo2 != NULL)
+        if (m_servo_1 != NULL)
         {
-          m_servo2->stopAndJoin();
-          delete m_servo2;
-          m_servo2 = NULL;
+          m_servo_1->stopAndJoin();
+          delete m_servo_1;
+          m_servo_1 = NULL;
         }
-        if (m_servo3 != NULL)
+        if (m_servo_2 != NULL)
         {
-          m_servo3->stopAndJoin();
-          delete m_servo3;
-          m_servo3 = NULL;
+          m_servo_2->stopAndJoin();
+          delete m_servo_2;
+          m_servo_2 = NULL;
         }
       }
 
@@ -162,25 +162,25 @@ namespace Actuators
         idServo = msg->id;
         if (!m_wrong_mode)
         {
-          if(idServo == 1)
+          if(idServo == 0)
+          {
+            //war("SERVO 0: %f", valuePos);
+            m_servo_0->SetPwmValue(valuePos);
+            if(!m_servo_0->CheckGPIOSate())
+              m_pwm_state = false;
+          }
+          else if(idServo == 1)
           {
             //war("SERVO 1: %f", valuePos);
-            m_servo1->SetPwmValue(valuePos);
-            if(!m_servo1->CheckGPIOSate())
+              m_servo_1->SetPwmValue(valuePos);
+            if(!m_servo_1->CheckGPIOSate())
               m_pwm_state = false;
           }
           else if(idServo == 2)
           {
             //war("SERVO 2: %f", valuePos);
-              m_servo2->SetPwmValue(valuePos);
-            if(!m_servo2->CheckGPIOSate())
-              m_pwm_state = false;
-          }
-          else if(idServo == 3)
-          {
-            //war("SERVO 3: %f", valuePos);
-            m_servo3->SetPwmValue(valuePos);
-            if(!m_servo3->CheckGPIOSate())
+            m_servo_2->SetPwmValue(valuePos);
+            if(!m_servo_2->CheckGPIOSate())
               m_pwm_state = false;
           }
         }
