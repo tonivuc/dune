155278400e63be9f20e2e07b92f8dc7ad6539cc9
Pedro Gonçalves <pedro@lsts.pt>
2016-07-07 14:26:56 +0100

    Maneuver/ImageTracking: add option to approach to the object detected.

--- src/Maneuver/ImageTracking/Task.cpp
+++ src/Maneuver/ImageTracking/Task.cpp
@@ -34,10 +34,16 @@ namespace Maneuver
   {
     using DUNE_NAMESPACES;
 
+    static const unsigned int c_maximum_distance = 12;
+    static const float c_time_check_new_distance = 2.0f;
+    static const int c_max_image_offset = 260;
+    static const int c_mim_image_offset = 60;
+
     enum MoveComand
     {
       M_LEFT,
       M_RIGHT,
+      M_FORWARD,
       M_STOP
     };
 
@@ -53,6 +59,8 @@ namespace Maneuver
         int maximum_x;
         //! Speed scale factor
         float speed_scale_factor;
+        //! Minimum distance before stopping moving forward
+        float minimum_distance;
     };
 
     struct Task: public DUNE::Maneuvers::Maneuver
@@ -60,7 +68,12 @@ namespace Maneuver
       IMC::SetThrusterActuation m_thruster_actuation;
       //! Task Arguments
       Arguments m_args;
-      bool m_set_estimated_state;
+      //! Distance to object
+      float m_distance_to_object;
+      //! State of new value of distance
+      bool m_new_distance;
+      //! state time to check state of new distances
+      Time::Counter<float> m_cnt_new_distance_check;
 
       Task(const std::string& name, Tasks::Context& ctx):
         DUNE::Maneuvers::Maneuver(name, ctx)
@@ -76,33 +89,66 @@ namespace Maneuver
 
         param("Minimum of X", m_args.minimum_x)
         .description("Minimum value of X before start correction of heading.")
+        .units(Units::Pixel)
         .minimumValue("60")
         .maximumValue("140")
-        .defaultValue("120");
+        .defaultValue("130");
 
         param("Maximum of X", m_args.maximum_x)
         .description("Maximum value of X before start correction of heading.")
+        .units(Units::Pixel)
         .minimumValue("260")
         .maximumValue("220")
-        .defaultValue("200");
+        .defaultValue("190");
 
         param("Speed Scaling Factor", m_args.speed_scale_factor)
         .description("Scaling factor for speed actuation.")
-        .minimumValue("1")
+        .minimumValue("0.5")
         .maximumValue("2")
+        .defaultValue("1.5");
+
+        param("Minimum Distance", m_args.minimum_distance)
+        .description("Minimum distance before stopping moving forward.")
+        .units(Units::Meter)
+        .minimumValue("0.5")
+        .maximumValue("12")
         .defaultValue("1.5");
 
         bindToManeuver<Task, IMC::ImageTracking>();
         bind<IMC::GetImageCoords>(this);
+        bind<IMC::GetWorldCoordinates>(this);
+      }
+
+      void
+      onManeuverDeactivation(void)
+      {
+        moveComand(M_STOP, 0);
       }
 
       void
       consume(const IMC::ImageTracking* maneuver)
       {
         (void)maneuver;
+        m_new_distance = false;
+        m_cnt_new_distance_check.setTop(c_time_check_new_distance);
 
-        m_set_estimated_state = true;
         signalProgress();
+      }
+
+      void
+      consume(const IMC::GetWorldCoordinates* msg)
+      {
+        if (std::string(resolveSystemId(msg->getSource())).compare(m_args.slave_name) != 0)
+          return;
+
+        if (!msg->tracking)
+          return;
+
+        if (msg->x >= m_args.minimum_distance && msg->x <= c_maximum_distance)
+        {
+          m_distance_to_object = msg->x;
+          m_new_distance = true;
+        }
       }
 
       void
@@ -115,11 +161,25 @@ namespace Maneuver
           return;
 
         if (msg->x < m_args.minimum_x)
+        {
           moveComand(M_LEFT, std::abs(msg->x - m_args.minimum_x));
+        }
         else if (msg->x > m_args.maximum_x)
+        {
           moveComand(M_RIGHT, std::abs(msg->x - m_args.maximum_x));
+        }
         else
-          moveComand(M_STOP, 0);
+        {
+          if (m_new_distance)
+          {
+            moveComand(M_FORWARD, m_distance_to_object);
+            m_new_distance = false;
+            m_cnt_new_distance_check.reset();
+          }
+
+          if (m_cnt_new_distance_check.overflow())
+            moveComand(M_STOP, 0);
+        }
       }
 
       void
@@ -130,7 +190,7 @@ namespace Maneuver
         switch (m_move)
         {
           case M_LEFT:
-            value = (diff_value / std::abs(60 - m_args.minimum_x)) * m_args.speed_scale_factor;
+            value = (diff_value / std::abs(c_mim_image_offset - m_args.minimum_x)) * m_args.speed_scale_factor;
             if (value > 1)
               value = 1;
             speed_values[0] = -value;
@@ -138,11 +198,19 @@ namespace Maneuver
             break;
 
           case M_RIGHT:
-            value = (diff_value / std::abs(260 - m_args.maximum_x)) * m_args.speed_scale_factor;
+            value = (diff_value / std::abs(c_max_image_offset - m_args.maximum_x)) * m_args.speed_scale_factor;
             if (value > 1)
               value = 1;
             speed_values[0] = value;
             speed_values[1] = -value;
+            break;
+
+          case M_FORWARD:
+            value = (diff_value / (c_maximum_distance / 2)) * m_args.speed_scale_factor;
+            if (value > 1)
+              value = 1;
+            speed_values[0] = value;
+            speed_values[1] = value;
             break;
 
           case M_STOP:
