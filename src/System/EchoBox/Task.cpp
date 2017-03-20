e58c31723c541d3e0276cc59a930d57b5c5ceec3
Pedro Gonçalves <pedro@lsts.pt>
2017-03-20 19:11:56 +0000

    System/EchoBox: add option for path config time file (CTF) and operation mode.

--- src/System/EchoBox/Task.cpp
+++ src/System/EchoBox/Task.cpp
@@ -27,6 +27,12 @@
 // Author: Pedro Gonçalves                                                   *
 //***************************************************************************
 
+// ISO C++ 98 headers.
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
 
@@ -36,32 +42,51 @@ namespace System
   {
     using DUNE_NAMESPACES;
 
+    //! Task arguments.
+    struct Arguments
+    {
+      //! Change mode of task (true - echobox, false - other system).
+      bool server_mode;
+      //! Path to config file of time
+      std::string path_config_time;
+      //! Number to skip in read of config file time.
+      int skip_lines;
+    };
+
     struct Task: public DUNE::Tasks::Task
     {
+      //! Task arguments.
+      Arguments m_args;
+      //! Path of config file DUNE
+      Tasks::Context m_path_ctx;
+      //! Path of config time file of echobox
+      Path m_path_cfg;
+      //! Path of log dir of system
+      Path m_path_log;
+      //! File of working time
+      std::ifstream m_file_time;
+      //! File to save info of working time
+      std::ofstream m_file_log_time;
+
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
         DUNE::Tasks::Task(name, ctx)
       {
-      }
+        param("Server Mode", m_args.server_mode)
+        .defaultValue("true")
+        .description("Change mode of task, true - echobox, false - other system.");
 
-      //! Update internal state with new parameter values.
-      void
-      onUpdateParameters(void)
-      {
-      }
+        param("Path Config Time File", m_args.path_config_time)
+        .defaultValue("hardware/echobox-time.ini")
+        .description("Path of Config Time File");
 
-      //! Reserve entity identifiers.
-      void
-      onEntityReservation(void)
-      {
-      }
-
-      //! Resolve entity names.
-      void
-      onEntityResolution(void)
-      {
+        param("Number Lines to Skip CTF", m_args.skip_lines)
+        .defaultValue("30")
+        .minimumValue("0")
+        .maximumValue("100")
+        .description("Number Lines to Skip in config time file.");
       }
 
       //! Acquire resources.
@@ -75,12 +100,135 @@ namespace System
       void
       onResourceInitialization(void)
       {
+        Time::Delay::wait(1.0);
+        m_path_log = m_path_ctx.dir_log / getSystemName() / "echobox_time_log.txt";
+        m_path_cfg = m_path_ctx.dir_cfg / m_args.path_config_time;
+
+        if(file_exist(m_path_cfg.c_str()))
+        {
+          if(file_exist(m_path_log.c_str()))
+          {
+            open_file(m_path_log.c_str(), false);
+          }
+          else
+          {
+            war("File log not exists, creating new, zero config");
+            open_file(m_path_log.c_str(), false);
+          }
+
+          war("new time: %s", get_new_data_time().c_str());
+          m_file_log_time.close();
+        }
+        else
+        {
+          err("Config File not exists");
+          setEntityState(IMC::EntityState::ESTA_ERROR, Utils::String::str("ERRO reading cfg time file"));
+          throw RestartNeeded("Config File not exists", 10);
+        }
+
+        debug("CFG  %s", m_path_cfg.c_str());
+        debug("LOG  %s", m_path_log.c_str());
+        debug("ALL  %s", Time::Format::getTimeDate().c_str());
+        debug("DATE %s", Time::Format::getDateSafe().c_str());
+        debug("TIME %s", Time::Format::getTimeSafe().c_str());
       }
 
       //! Release resources.
       void
       onResourceRelease(void)
       {
+        try
+        {
+          m_file_time.close();
+          m_file_log_time.close();
+        }
+        catch(...)
+        {}
+      }
+
+      std::string
+      get_new_data_time(void)
+      {
+        std::string new_data;
+        std::string line;
+        int number_lines_log = get_number_lines(m_path_log.c_str());
+        int number_lines_cfg = get_number_lines(m_path_cfg.c_str()) - 30;
+
+        open_file(m_path_cfg.c_str(), true);
+
+        for(int i = 0; i < m_args.skip_lines ; i++)
+          getline (m_file_time,line);
+
+        if(number_lines_log == 0)
+        {
+          getline (m_file_time,line);
+          m_file_log_time << line.c_str() << std::endl;
+        }
+        else if(number_lines_log < number_lines_cfg)
+        {
+          for(int i = 0; i <= number_lines_log; i++)
+          {
+            getline (m_file_time,line);
+            if(i == number_lines_log)
+              m_file_log_time << line.c_str() << std::endl;
+          }
+        }
+        else
+        {
+          war("now new data in cfg file, adding clock info");
+          m_file_log_time << Time::Format::getTimeDate().c_str() << std::endl;
+          return Time::Format::getTimeDate().c_str();
+        }
+
+        m_file_log_time.close();
+
+        return line;
+      }
+
+      int
+      get_number_lines(std::string file_path)
+      {
+        int cnt_lines = 0;
+        std::string line;
+
+        m_file_time.open(file_path.c_str());
+        if (m_file_time.is_open())
+        {
+          while ( getline (m_file_time,line) )
+          {
+            cnt_lines++;
+          }
+          m_file_time.close();
+        }
+        return cnt_lines;
+      }
+
+      bool
+      open_file(std::string path_file, bool toRead)
+      {
+        if(toRead)
+        {
+          m_file_time.open(path_file.c_str());
+          return m_file_time.is_open();
+        }
+        else
+        {
+          m_file_log_time.open(path_file.c_str(), std::ios_base::app);
+          m_file_time.is_open();
+        }
+
+        return false;
+      }
+
+      bool
+      file_exist(std::string file_path)
+      {
+        if (std::ifstream(file_path.c_str()))
+          return true;
+        else
+          return false;
+
+        return false;
       }
 
       //! Main loop.
@@ -90,6 +238,8 @@ namespace System
         while (!stopping())
         {
           waitForMessages(1.0);
+          /*Time::Delay::wait(5.0);
+          throw RestartNeeded("for test", 2);*/
         }
       }
     };
