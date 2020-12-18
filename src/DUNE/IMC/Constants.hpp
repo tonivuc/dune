484ac92ec6d5bc5197a4fcde46b144dc518c45cf
Pedro Gonçalves <pedro@lsts.pt>
2020-12-18 14:20:29 +0000

    Src/DUNE/IMC/Constants: fix header info of imc git.

--- src/DUNE/IMC/Constants.hpp
+++ src/DUNE/IMC/Constants.hpp
@@ -37,7 +37,7 @@
 //! IMC version string.
 #define DUNE_IMC_CONST_VERSION "5.4.11"
 //! Git repository information.
-#define DUNE_IMC_CONST_GIT_INFO "2020-12-16 f19a343  (HEAD -> master, origin/master, origin/HEAD)"
+#define DUNE_IMC_CONST_GIT_INFO "2020-12-18 6704886  (HEAD -> feature/pointcam, origin/feature/pointcam)"
 //! MD5 sum of XML specification file.
 #define DUNE_IMC_CONST_MD5 "5563261871977bdb0b45e3d1e53b575b"
 //! Synchronization number.
