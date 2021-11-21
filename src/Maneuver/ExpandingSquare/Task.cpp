c71e6d6d42b6d2ad5fcbed7693aa1cd8712f7a9f
Toni Vucic <tonivuc@gmail.com>
2021-11-21 23:14:10 +0100

    Merged code from other branch with ExpandingSquare task skeleton. It is now an instance of Maneuver, not Task directly

--- src/Maneuver/ExpandingSquare/Task.cpp
+++ src/Maneuver/ExpandingSquare/Task.cpp
@@ -40,53 +40,227 @@ namespace Maneuver
   {
     using DUNE_NAMESPACES;
 
-    struct Task: public DUNE::Tasks::Task
+    struct Task: public Maneuvers::Maneuver
     {
+      //! Maneuver
+      IMC::ExpandingSquare m_maneuver;
+      //! EstimatedState
+      IMC::EstimatedState m_state;
+      //! DesiredPath
+      IMC::DesiredPath m_path;
+
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
-        DUNE::Tasks::Task(name, ctx)
+        DUNE::Maneuvers::Maneuver(name, ctx)
       {
-        war("ExpandingSquare test");
+        war("Inside ExpandingSquare task constructor");
+        bindToManeuver<Task, IMC::ExpandingSquare>();
+        //paramActive(Tasks::Parameter::SCOPE_GLOBAL, Tasks::Parameter::VISIBILITY_USER, true); //Required to support task activation and deactivation
       }
 
-      //! Update internal state with new parameter values.
       void
-      onUpdateParameters(void)
+      consume(const IMC::ExpandingSquare* maneuver)
       {
+        if (maneuver->getSource() != getSystemId())
+          return;
+
+        m_maneuver = *maneuver;
+
+
+        //New code!
+
+        //Send first DesiredPath based on manouver parameters.
+
+        //Get current location
+
+        //sendPath(lat, lon)
+
+        //Store remaining points in an array in the object? (Not the best way, better to have a function always giving the next point, but this is easier)
+
+        //Old code!!
+        /*
+        double hstep;
+        if (maneuver->angaperture <= 0)
+          hstep = 2 * maneuver->range;
+        else
+          hstep = 2 * maneuver->range * std::sin(maneuver->angaperture / 2);
+
+        m_alt_min = -1;
+        m_cov_pred = hstep * (1 - maneuver->overlap / 200.);
+        m_cov_actual_min = m_cov_pred;
+        m_cur_hstep = m_cov_pred;
+
+        m_stage = 0;
+
+        // Creates a new instance as this is the only way to change window size
+        Memory::clear(m_alt_avrg);
+        m_alt_avrg = new Math::MovingAverage<float>(m_args.altitude_average_size);
+
+        Memory::clear(m_stages_parser);
+        try
+        {
+          m_stages_parser = new Maneuvers::RowsStages(maneuver->lat, maneuver->lon, maneuver->bearing,
+                                                      maneuver->cross_angle, maneuver->width,
+                                                      maneuver->length, hstep, maneuver->coff, 100,
+                                                      maneuver->flags, this);
+        }
+        catch (std::runtime_error& e)
+        {
+          signalError(e.what());
+          return;
+        }
+
+        setControl(IMC::CL_PATH);
+        m_path.speed = maneuver->speed;
+        m_path.speed_units = maneuver->speed_units;
+        m_path.end_z = maneuver->z;
+        m_path.end_z_units = maneuver->z_units;
+
+        double lat;
+        double lon;
+
+        if (m_stages_parser->getFirstPoint(&lat, &lon))
+        {
+          signalCompletion();
+          return;
+        }
+
+        sendPath(lat, lon);
+        */
       }
 
-      //! Reserve entity identifiers.
       void
-      onEntityReservation(void)
+      consume(const IMC::EstimatedState* msg)
       {
+        if (msg->getSource() != getSystemId())
+          return;
+
+        /*
+        if (m_alt_avrg == NULL)
+          return;
+        */
+
+        //New code!
+        fp64_t lat = msg->lat;
+        fp64_t lon = msg->lon;
+
+        war("Estimated latitude and longtitude are %f and %f", lat, lon);
+
+        //Old code
+        /*
+        if (msg->alt > m_args.min_altitude)
+        {
+          m_alt_avrg->update(msg->alt);
+          if (m_alt_avrg->sampleSize() >= m_alt_avrg->windowSize())
+          {
+            float avg = m_alt_avrg->mean();
+            if (m_alt_min < 0)
+              m_alt_min = avg;
+            else
+              m_alt_min = std::min(m_alt_min, avg);
+          }
+        }
+        */
       }
 
-      //! Resolve entity names.
+      //! On PathControlState message
+      //! @param[in] pcs pointer to PathControlState message
       void
-      onEntityResolution(void)
+      onPathControlState(const IMC::PathControlState* pcs)
       {
+        inf("Inside onPathControlState");
+        /*
+        if (m_alt_avrg == NULL)
+          return;
+        */
+
+
+        //New code!
+        
+        //Check if close to/reached point (can set a static 3m radius for close enough)
+        //If close enough send the next waypoint
+
+
+        //Old code!
+        /*
+        std::stringstream ss;
+        ss << "waypoint=" << m_stages_parser->getIndex();
+        ss << "; stage=" << m_stage;
+        ss << "; minAlt=" << m_alt_min;
+        ss << "; curHstep=" << m_cur_hstep;
+        ss << "; stdHstep=" << m_cov_pred;
+
+
+        if (!(pcs->flags & IMC::PathControlState::FL_NEAR))
+        {
+          signalProgress(pcs->eta, ss.str());
+          return;
+        }
+
+        double lat;
+        double lon;
+
+        bool last_pt;
+        switch (m_stage)
+        {
+          case 2:
+            m_alt_min = std::min(m_alt_min, m_alt_avrg->mean());
+            if (m_alt_min > 0)
+            {
+              m_cov_actual_min = 2 * m_alt_min * std::tan(m_maneuver.angaperture / 2);
+              m_cov_actual_min = m_cov_actual_min * (1 - m_maneuver.overlap / 200.);
+              m_cur_hstep = std::min(m_cov_pred, m_cov_actual_min);
+              last_pt = m_stages_parser->getNextPoint(&lat, &lon, m_cur_hstep);
+              ss << "; calculated=" << m_cur_hstep;
+            }
+            else
+            {
+              m_cur_hstep = m_cov_pred;
+              last_pt = m_stages_parser->getNextPoint(&lat, &lon, m_cur_hstep);
+              ss << "; not-calculated=" << m_cov_pred;
+            }
+            break;
+          default:
+            last_pt = m_stages_parser->getNextPoint(&lat, &lon);
+            break;
+        }
+
+        signalProgress(pcs->eta, ss.str());
+
+        m_alt_avrg->clear();
+        m_alt_min = -1;
+
+        if (last_pt)
+        {
+          signalCompletion();
+          return;
+        }
+
+        m_stage++;
+        unsigned num_stages = (m_maneuver.flags & IMC::ExpandingSquare::FLG_SQUARE_CURVE) ? 3 : 2;
+
+        if (m_stage > num_stages)
+          m_stage = 1;
+
+        sendPath(lat, lon);
+        */
       }
 
-      //! Acquire resources.
+      //! Send new desired path as a DesiredPath IMC message
+      //! @param[in] lat latitude for new desired path
+      //! @param[in] lon longitude for new desired path
       void
-      onResourceAcquisition(void)
+      sendPath(double lat, double lon)
       {
+        // Calculate WGS-84 coordinates and fill DesiredPath message
+        m_path.end_lat = lat;
+        m_path.end_lon = lon;
+        m_path.flags = 0;
+        dispatch(m_path);
       }
-
-      //! Initialize resources.
-      void
-      onResourceInitialization(void)
-      {
-      }
-
-      //! Release resources.
-      void
-      onResourceRelease(void)
-      {
-      }
-
+      
       //! Main loop.
       void
       onMain(void)
